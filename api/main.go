61c05025533c1ab840ff92a1ac2c3e142a7cf06a
Sesha Andipa <sesha04@gmail.com>
2020-12-05 17:12:41 +0700

    First Commit

---
+++ api/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", hello)
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func(s *http.Server) {
+		log.Printf("apps is available at %s\n", s.Addr)
+		if serr := s.ListenAndServe(); serr != http.ErrServerClosed {
+			log.Fatal(serr)
+		}
+	}(s)
+
+	<-sigChan
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sesha04/test_kumparan
+
+go 1.12
+
+require github.com/gorilla/mux v1.8.0 // indirect
